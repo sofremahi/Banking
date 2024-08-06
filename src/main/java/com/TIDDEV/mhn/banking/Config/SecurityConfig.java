9023f94ec9d623c839e93475de28946c9f019971
mahi <156359781+sofremahi@users.noreply.github.com>
2024-08-06 16:21:59 +0330

    added spring security
    admin and user

---
+++ src/main/java/com/TIDDEV/mhn/banking/Config/SecurityConfig.java
@@ -0,0 +1,60 @@
+package com.TIDDEV.mhn.banking.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+// letting all people reach any endpoint
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .csrf().disable()
+//                .authorizeRequests()
+//                .antMatchers("/rest/**").authenticated()
+//                .anyRequest().permitAll()
+//                .and()
+//                .httpBasic(); // enable basic authentication
+//    }
+@Override
+protected void configure(HttpSecurity http) throws Exception {
+    http
+            .csrf().disable()
+            .authorizeRequests()
+            .antMatchers("/rest/**").hasRole("ADMIN")
+            .antMatchers("/rest/banking/account/find").hasRole("USER")
+            .anyRequest().authenticated()
+            .and()
+            .httpBasic();
+}
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("username")
+                .password(passwordEncoder().encode("password"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder().encode("password"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user , admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
